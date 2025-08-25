1c14807459b1973413fc0ff7cce5f73eebdc7c52
DominikMarciniszyn <dominik.marciniszyn@codetain.com>
2025-08-25 09:53:28 +0200

    feat: update alternatives structure

--- model/smartenergymanagementps.go
+++ model/smartenergymanagementps.go
@@ -60,7 +60,9 @@ type SmartEnergyManagementPsPowerTimeSlotValueListElementsType struct {
 
 type SmartEnergyManagementPsDataType struct {
 	NodeScheduleInformation *PowerSequenceNodeScheduleInformationDataType `json:"nodeScheduleInformation,omitempty"` // ignoring changes
-	Alternatives            [][]SmartEnergyManagementPsAlternativesType   `json:"alternatives,omitempty"`
+	Alternatives            [][]struct {
+		PowerSequence [][]SmartEnergyManagementPsPowerSequenceType `json:"powerSequence,omitempty"`
+	} `json:"alternatives,omitempty"`
 }
 
 type SmartEnergyManagementPsDataElementsType struct {
